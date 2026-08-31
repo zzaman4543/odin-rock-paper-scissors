d6e0dd02da9f3ba0b2b50ffa62e54ac5c8b6d553
Zeeshan Zaman <169604869+zzaman4543@users.noreply.github.com>
2026-08-31 21:00:24 +0100

    Write pseudocode for getting random computer choice

---
+++ main.js
@@ -0,0 +1,6 @@
+//Create a function called getComputerChoice
+//Generate a number between 1 and 3
+//Initialise a variable with the generated number
+//If number is 1, return rock
+//If number is 2, return paper
+//If number is 3, return scissors
