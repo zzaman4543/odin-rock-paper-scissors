aadf999a3e5a8f318ae6b3516de6a6c2c842382b
Zeeshan Zaman <169604869+zzaman4543@users.noreply.github.com>
2026-08-31 21:04:59 +0100

    Fix pseudocode for initalising random number variable

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 //Create a function called getComputerChoice
-//Generate a number between 1 and 3
-//Initialise a variable with the generated number
+
+//Initialise a variable with a randomly generated number between 1 and 3
+
 //If number is 1, return rock
+
 //If number is 2, return paper
+
 //If number is 3, return scissors
+
+
