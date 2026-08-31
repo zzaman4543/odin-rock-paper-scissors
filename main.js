8bc425abe0ec67829bbf436f16c1adb70436a6c3
Zeeshan Zaman <169604869+zzaman4543@users.noreply.github.com>
2026-08-31 21:25:03 +0100

    Convert pseudocode for generating computer choice to code

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 //Create a function called getComputerChoice
+function getComputerChoice() {
 
 //Initialise a variable with a randomly generated number between 1 and 3
+generatedNumber = Math.floor(Math.random() * 3) + 1;
 
 //If number is 1, return rock
+if (generatedNumber === 1) {
+    return "rock";
 
 //If number is 2, return paper
+} else if (generatedNumber === 2) {
+    return "paper";
 
 //If number is 3, return scissors
-
+} else {
+        return "scissors";
+    }
+}
 
