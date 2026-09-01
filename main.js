ffbe7f0cba6f231c427b01b1e726402a43d3cb44
Zeeshan Zaman <169604869+zzaman4543@users.noreply.github.com>
2026-09-01 21:01:33 +0100

    Add proper indentation

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //Create a function called getComputerChoice
 function getComputerChoice() {
 
-//Initialise a variable with a randomly generated number between 1 and 3
-generatedNumber = Math.floor(Math.random() * 3) + 1;
+    //Initialise a variable with a randomly generated number between 1 and 3
+    generatedNumber = Math.floor(Math.random() * 3) + 1;
 
-//If number is 1, return rock
-if (generatedNumber === 1) {
-    return "rock";
+    //If number is 1, return rock
+    if (generatedNumber === 1) {
+        return "rock";
 
-//If number is 2, return paper
-} else if (generatedNumber === 2) {
-    return "paper";
+    //If number is 2, return paper
+    } else if (generatedNumber === 2) {
+        return "paper";
 
-//If number is 3, return scissors
-} else {
+    //If number is 3, return scissors
+    } else {
         return "scissors";
     }
 }
@@ -21,9 +21,9 @@ if (generatedNumber === 1) {
 //Create a function called getHumanChoice
 function getHumanChoice() {
 
-//Initialise variable with user choice
-userChoice = prompt("Please enter your choice");
+    //Initialise variable with user choice
+    userChoice = prompt("Please enter your choice");
 
-//Return user choice
-return userChoice;
+    //Return user choice
+    return userChoice;
 }
